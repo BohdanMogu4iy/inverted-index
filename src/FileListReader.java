82c11f751434ea75c6ae7ddc396c036811036fd2
Bohdan <mogu4iy.kotygoroshko@gmail.com>
2021-05-24 17:43:49 +0300

    Files reader created

--- src/FileListReader.java
+++ src/FileListReader.java
@@ -1,3 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class FileListReader {
 
+    ArrayList<File> fileList = new ArrayList<File>();
+
+    FileListReader(String dataDir){
+        Path dir = Paths.get(dataDir);
+        try {
+            Files.walk(dir).forEach(path -> createList(path.toFile()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createList(File file) {
+        if (!file.isDirectory()) {
+            this.fileList.add(file);
+        }
+    }
+
+    public ArrayList<File> getFiles(){
+        return fileList;
+    }
 }
