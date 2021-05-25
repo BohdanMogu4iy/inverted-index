1d658b59ebd3953d4cb9c2953c58ab272ce4b6a0
Bohdan <mogu4iy.kotygoroshko@gmail.com>
2021-05-25 12:37:01 +0300

    fix(client): added Communication protocol (1 byte for length, other bytes for data)

--- src/ClientTCP.java
+++ src/ClientTCP.java
@@ -1,8 +1,6 @@
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CompletableFuture;
@@ -11,8 +9,8 @@ import java.util.concurrent.ExecutionException;
 public class ClientTCP {
 
     Socket socket;
-    InputStream inputStream;
-    OutputStream outputStream;
+    DataInputStream inputStream;
+    DataOutputStream outputStream;
 
     int clientId;
     String serverName;
@@ -27,15 +25,16 @@ public class ClientTCP {
     public void connect() throws IOException {
         //  Создаем сокет для подальшего использования
         socket = new Socket(serverName, port);
-        inputStream = socket.getInputStream();
-        outputStream = socket.getOutputStream();
+        inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        outputStream =  new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
         System.out.println("Client [" + clientId + "] connected to " + socket.getRemoteSocketAddress());
     }
 
     public void closeConnection() throws IOException {
+        System.out.println("Client [" + clientId + "] closed connection to " + socket.getRemoteSocketAddress());
         //  Закрываем сокет
         socket.close();
-        System.out.println("Client [" + clientId + "] closed connection to " + socket.getRemoteSocketAddress());
     }
 
     public JSONObject request(JSONObject Data) throws IOException, InterruptedException, ExecutionException {
@@ -70,27 +69,20 @@ public class ClientTCP {
     jsonInput input = () -> CompletableFuture
             .supplyAsync(
                     () -> {
-                        JSONObject result = null;
-                        float count = -1;
-                        while (count == -1) {
-                            try {
-                                byte[] data = new byte[1000];
-                                // Прием данных из InputStream
-                                count = inputStream.read(data);
-                                if (count == -1) {
-                                    // Если данные пустые мы пробуем еще раз
-                                    continue;
-                                }
-                                // Записываем данные в JSONObject
-                                result = new JSONObject(new String(data, StandardCharsets.UTF_8));
-                                System.out.println("Client [" + clientId + "] got response from " + socket.getRemoteSocketAddress() + "\n" + "Response :\n" + result.toString(2));
-                            } catch (IOException e) {
-                                System.err.println("Client [" + clientId + "] Error : " + e);
-                                result = null;
-                            }
+                        JSONObject responseJson = null;
+                        try {
+                            int length = inputStream.readInt();
+                            byte[] responseData = new byte[length];
+                            inputStream.readFully(responseData);
+                            responseJson = new JSONObject(new String(responseData, StandardCharsets.UTF_8));
+
+                            System.out.println("Client [" + clientId + "] got response from " + socket.getRemoteSocketAddress() + "\n" + "Response :\n" + responseJson.toString(2));
+                        } catch (IOException e) {
+                            System.err.println("Client [" + clientId + "] Error : " + e);
+                            responseJson = null;
+                            e.printStackTrace();
                         }
-                        // Возвращаем response
-                        return result;
+                        return responseJson;
                     }
             );
 
@@ -98,12 +90,16 @@ public class ClientTCP {
             .supplyAsync(
                     () -> {
                         try {
-                            // Считываем данные из inputStream
+                            byte[] requestData = Data.toString().getBytes(StandardCharsets.UTF_8);
                             System.out.println("Client [" + clientId + "] sent request to " + socket.getRemoteSocketAddress() + "\n" + "Request :\n" + Data.toString(2));
-                            outputStream.write(Data.toString().getBytes(StandardCharsets.UTF_8));
+
+                            outputStream.writeInt(requestData.length);
+                            outputStream.write(requestData);
+                            outputStream.flush();
                             return true;
                         } catch (IOException e) {
                             System.err.println("Client [" + clientId + "] Error : " + e);
+                            e.printStackTrace();
                             return false;
                         }
                     }
